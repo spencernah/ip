d5cac914606d02c5be2811b79b0605c92b3951f2
WANG WENWEI <e0373972@u.nus.edu>
2021-08-17 01:08:00 +0800

    Update Duke.java
    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,17 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        
+        printWithLine(List.of("Hello from\n" + logo));
+        printWithLine(List.of("Hello! I'm Duke", "What can I do for you?"));
+        printWithLine(List.of("Bye. Hope to see you again soon!"));
+    }
+
+
+    private static void printWithLine(List<String> messages){
+        for (String message : messages){
+            System.out.println("   " + message);
+        }
+        System.out.println("   _____________________________________");
     }
 }
