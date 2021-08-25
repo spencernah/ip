e2d980e5e641965eac13f4adddc33c4a6db43c0b
WANG WENWEI <e0373972@u.nus.edu>
2021-08-25 23:52:57 +0800

    branch-Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,43 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-	
+
+    static void checkLineEmpty(String line)throws DukeCheckLineEmptyException{
+        if (line.equals("")){
+            throw new DukeCheckLineEmptyException();
+        }
+    }
+
+    static void checkWord(String line)throws DukeCheckLineException{
+        String keyword = line.split(" ")[0].toLowerCase();
+
+        if (!keyword.equals("list") && !keyword.equals("bye")
+        && !keyword.equals("todo") && !keyword.equals("done")
+        && !keyword.equals("event") && !keyword.equals("deadline")){
+            throw new DukeCheckLineException();
+        }
+    }
+
+    static void checkDescription(String line)throws DukeException{
+        String keyword = line.split(" ")[0].toLowerCase();
+
+        if (keyword.equals("todo") && line.split(" ").length == 1){
+            throw new DukeException();
+        }
+
+        if (keyword.equals("event") && line.split(" ").length == 1){
+            throw new DukeException();
+        }
+
+        if (keyword.equals("deadline") && line.split(" ").length == 1){
+            throw new DukeException();
+        }
+
+        if (keyword.equals("done") && line.split(" ").length == 1){
+            throw new DukeException();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,54 +56,74 @@ public class Duke {
 
         do{
             line = in.nextLine();
-            if(line.toLowerCase().equals("list")){
-                printWithLine(List.of());
-                List<String>messages = new ArrayList<>();
-                System.out.println("   Here are the tasks in your list: ");
-                for (int i = 0; i < item.size(); i++){
-                    messages.add(i + 1 + "." + item.get(i));
+            String keyword = line.split(" ")[0].toLowerCase();
+
+            try {
+                checkLineEmpty(line);
+                checkWord(line);
+                checkDescription(line);
+
+                if (line.toLowerCase().equals("list")) {
+                    printWithLine(List.of());
+                    List<String> messages = new ArrayList<>();
+                    System.out.println("   Here are the tasks in your list: ");
+                    for (int i = 0; i < item.size(); i++) {
+                        messages.add(i + 1 + "." + item.get(i));
+                    }
+                    printWithLine(messages);
+
+                } else if (line.split(" ")[0].toLowerCase().equals("done")) {
+                    printWithLine(List.of());
+                    Task markItem = item.get(Integer.parseInt(line.substring(5)) - 1);
+                    markItem.markAsDone();
+                    printWithLine(List.of("Nice! I've marked this task as done: ", " " + markItem));
+
+                } else if (line.split(" ")[0].toLowerCase().equals("todo")) {
+                    printWithLine(List.of());
+                    Task todoTask = new Todo(line.replace(line.split(" ")[0] + " ", ""));
+                    item.add(todoTask);
+                    printWithLine((List.of("Got it. I've added this task: ", todoTask.toString(), "Now you have " + item.size() + " task in the list. ")));
+
+                } else if (line.split(" ")[0].toLowerCase().equals("deadline")) {
+                    printWithLine(List.of());
+                    int position = line.indexOf("/");
+                    String time = line.split("/")[1].replace("by ", "");
+                    Task deadlineTask = new Deadline(line.substring(9, position - 1), time);
+                    item.add(deadlineTask);
+                    printWithLine((List.of("Got it. I've added this task: ", deadlineTask.toString(), "Now you have " + item.size() + " task in the list. ")));
+
+                } else if (line.split(" ")[0].toLowerCase().equals("event")) {
+                    printWithLine(List.of());
+                    int position = line.indexOf("/");
+                    String time = line.split("/")[1].replace("at ", "");
+                    Task eventTask = new Event(line.substring(6, position - 1), time);
+                    item.add(eventTask);
+                    printWithLine((List.of("Got it. I've added this task: ", eventTask.toString(), "Now you have " + item.size() + " task in the list. ")));
+
+                } else if (line.toLowerCase().equals("bye")) {
+                    printWithLine(List.of());
+                    printWithLine(List.of("Bye. Hope to see you again soon!"));
+
+                } else {
+                    Task task = new Task(line);
+                    item.add(task);
+                    printWithLine(List.of());
+                    printWithLine(List.of("added: " + task));
                 }
-                printWithLine(messages);
-
-            } else if(line.split(" ")[0].toLowerCase().equals("done")){
-                printWithLine(List.of());
-                Task markItem = item.get(Integer.parseInt(line.substring(5)) - 1);
-                markItem.markAsDone();
-                printWithLine(List.of("Nice! I've marked this task as done: ", " " + markItem));
-
-            } else if(line.split(" ")[0].toLowerCase().equals("todo")){
-                printWithLine(List.of());
-                Task todoTask = new Todo(line.replace(line.split(" ")[0] + " ", ""));
-                item.add(todoTask);
-                printWithLine((List.of("Got it. I've added this task: ", todoTask.toString(), "Now you have " + item.size() + " task in the list. ")));
-
-            } else if(line.split(" ")[0].toLowerCase().equals("deadline")){
-                printWithLine(List.of());
-                int position = line.indexOf("/");
-                String time = line.split("/")[1].replace("by ", "");
-                Task deadlineTask = new Deadline(line.substring(9, position - 1), time);
-                item.add(deadlineTask);
-                printWithLine((List.of("Got it. I've added this task: ", deadlineTask.toString(), "Now you have " + item.size() + " task in the list. ")));
-
-            } else if(line.split(" ")[0].toLowerCase().equals("event")){
-                printWithLine(List.of());
-                int position = line.indexOf("/");
-                String time = line.split("/")[1].replace("at ", "");
-                Task eventTask = new Event(line.substring(6, position - 1), time);
-                item.add(eventTask);
-                printWithLine((List.of("Got it. I've added this task: ", eventTask.toString(), "Now you have " + item.size() + " task in the list. ")));
-
-            } else if(line.toLowerCase().equals("bye")){
-                printWithLine(List.of());
-                printWithLine(List.of("Bye. Hope to see you again soon!"));
-
-            } else{
-                Task task = new Task(line);
-                item.add(task);
-                printWithLine(List.of());
-                printWithLine(List.of("added: " + task));
-
             }
+            catch (DukeCheckLineException e){
+                printWithLine(List.of());
+                printWithLine(List.of("   ☹ OOPS!!! I'm sorry, but I don't know what that means :-("));
+            }
+            catch (DukeCheckLineEmptyException e){
+                printWithLine(List.of());
+                printWithLine(List.of("   ☹ OOPS!!! Please enter somethings."));
+            }
+            catch (DukeException e){
+                printWithLine(List.of());
+                printWithLine((List.of("    ☹ OOPS!!! The description of a " + keyword + " cannot be empty.")));
+            }
+
         }while(!line.equals("bye"));
     }
 
---
+++ src/main/java/DukeCheckLineEmptyException.java
@@ -0,0 +1,2 @@
+public class DukeCheckLineEmptyException extends Exception{
+}
