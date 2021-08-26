af4ecdf6974f072a5d132361128af814cfe7bca8
DESKTOP-3U485CE\User <spencernzh@gmail.com>
2021-08-26 21:58:37 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import ui.Ui;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+         */
+
+        Ui ui = new Ui();
+        ui.welcome();
+        ui.bye();
     }
 }
--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,3 +1,5 @@
+package ui;
+
 import java.util.Scanner;
 
 public class Ui {
@@ -7,6 +9,18 @@ public class Ui {
         in = new Scanner(System.in);
     }
 
+    public void newline(){
+        System.out.println("_____________________________________________________________");
+    }
+    public void welcome(){
+        newline();
+        System.out.println("Hellu, I ish Dook!");
+        newline();
+    }
 
+    public void bye(){
+        System.out.println("Buai :(");
+        newline();
+    }
 }
 
