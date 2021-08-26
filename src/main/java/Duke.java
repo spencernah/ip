5b434f97f25fc0391b2054b7539f3347e8fd2267
DESKTOP-3U485CE\User <spencernzh@gmail.com>
2021-08-27 00:31:13 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,19 @@
 import ui.Ui;
+import java.util.*;
 
 public class Duke {
+    protected static List<String> tasks = new ArrayList<String>();
+
+    public static void printList() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i+1 + ". " + tasks.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         boolean isExit = false;
+
+
         /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,15 +27,30 @@ public class Duke {
         ui.welcome();
         while (!isExit) {
             String command = ui.readCommand();
-            if (command.equals("bye")) {
-                isExit = true;
-                ui.bye();
+
+            switch(command) {
+                case "bye":
+                    isExit = true;
+                    ui.bye();
+                    break;
+                case "hello":
+                    ui.echo(command);
+                    break;
+                case "list":
+                    printList();
+                    ui.newline();
+                    /*
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i+1 + ". " + tasks.get(i));
+                    }
+                    */
+                    break;
+                default:
+                    ui.echo("added: " + command);
+                    tasks.add(command);
             }
-            else {
-                ui.echo(command);
-            }
+
         }
-
-
     }
 }
+
