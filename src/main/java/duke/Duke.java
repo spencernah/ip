891062311586251b201e7b1035d76e36195ec315
WANG WENWEI <e0373972@u.nus.edu>
2021-09-01 01:30:31 +0800

    Coding Standard

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,6 +1,9 @@
 package duke;
 
-import duke.task.*;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
