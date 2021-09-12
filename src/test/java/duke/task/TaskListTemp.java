b86cd9e660badbce786e4c886105a3982b404ef8
WANG WENWEI <e0373972@u.nus.edu>
2021-09-12 14:59:05 +0800

    A-JUnit: Add JUnit Tests

---
+++ src/test/java/duke/task/TaskListTemp.java
@@ -0,0 +1,33 @@
+package duke.task;
+
+import java.util.List;
+
+
+public class TaskListTemp extends TaskList {
+    public Task doTask;
+
+    @Override
+    public void add(Task task) {
+        doTask = task;
+    }
+
+    @Override
+    public Task remove(int indexNo) {
+        return doTask = new Task("remove " + indexNo);
+    }
+
+    @Override
+    public Task get(int indexNo) {
+        return doTask = new Task ("get " + indexNo);
+    }
+
+    @Override
+    public int size() {
+        return 1;
+    }
+
+    @Override
+    public List<String> convertAsLines() {
+        return List.of(doTask.toString());
+    }
+}
