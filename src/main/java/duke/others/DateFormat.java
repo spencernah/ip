9487404af6e829ea6954113ff964a16c7bebd8ef
DESKTOP-3U485CE\User <spencernzh@gmail.com>
2021-09-27 23:18:47 +0800

    Level-8

--- src/main/java/duke/others/DateFormat.java
+++ src/main/java/duke/others/DateFormat.java
@@ -1,7 +1,7 @@
 package duke.others;
 
 /**
- * Represents the different date format to be used in Duke.
+ * Represents the different date format to be used.
  */
 public class DateFormat {
     public static final String EVENT_AND_DEADLINE = "dd MMM yyyy";
