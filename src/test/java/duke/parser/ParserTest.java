960a08293056fc18445b058beb1baa5ea19cc9a8
DESKTOP-3U485CE\User <spencernzh@gmail.com>
2022-02-20 19:47:00 +0800

    A-CheckStyle

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -1,24 +1,28 @@
 package duke.parser;
 
-import duke.command.*;
-import duke.others.DukeException;
-import duke.others.Keyword;
-import duke.storage.Storage;
-import duke.task.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import duke.ui.Ui;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import duke.command.Command;
+import duke.others.DukeException;
+import duke.others.Keyword;
+import duke.storage.Storage;
+import duke.task.Deadline;
+import duke.task.TaskList;
+import duke.ui.Ui;
+
+
+
 
 public class ParserTest {
 
-    Ui ui = new Ui();
-    Storage storage = new Storage("/Users/spencernah/code/duke/data/data_test.txt");
-    TaskList tasks;
+    private Ui ui = new Ui();
+    private Storage storage = new Storage("/Users/spencernah/code/duke/data/data_test.txt");
+    private TaskList tasks;
 
     @BeforeEach
     public void setup() {
@@ -32,72 +36,71 @@ public class ParserTest {
     }
 
     @Test
-    public void parse_ViewByDateCommand_noDate() {
+    public void parseViewByDateCommandNoDate() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.VIEW_BY_DATE);
         });
     }
 
     @Test
-    public void parse_ViewByDateCommand_incorrectDateFormat() {
+    public void parseViewByDateCommandIncorrectDateFormat() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.VIEW_BY_DATE + "/20190805");
         });
     }
 
     @Test
-    public void parse_DoneCommand_incorrectIndex() {
+    public void parseDoneCommandIncorrectIndex() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse("done abc");
         });
     }
 
     @Test
-    public void parse_DeleteCommand_incorrectIndex() {
+    public void parseDeleteCommandIncorrectIndex() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse("delete abc");
         });
     }
 
     @Test
-    public void parse_AddCommand_deadline_noDesc() {
+    public void parseAddCommandDeadlineNoDesc() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.ADD_DEADLINE);
         });
     }
 
     @Test
-    public void parse_AddCommand_deadline_noDate() {
+    public void parseAddCommandDeadlineNoDate() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.ADD_DEADLINE + "test/");
         });
     }
 
     @Test
-    public void parse_AddCommand_deadline_incorrectDateFormat() {
+    public void parseAddCommandDeadlineIncorrectDateFormat() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.ADD_DEADLINE + "test/20190805");
         });
     }
 
     @Test
-    public void parse_AddCommand_todo_noDesc() {
+    public void parseAddCommandTodoNoDesc() {
         Assertions.assertThrows(DukeException.class, () -> {
             Command c = Parser.parse(Keyword.ADD_TODO);
         });
     }
 
     @Test
-    public void parse_AddCommand_Deadline_dateFormat() {
+    public void parseAddCommandDeadlineDateFormat() throws DukeException {
         String test = "";
         try {
             Command c = Parser.parse(Keyword.ADD_DEADLINE + "test/2019-8-5");
             c.execute(tasks, ui, storage);
-            test = ((Deadline) tasks.get(tasks.size()-1)).getFormattedDate();
-        } catch (DukeException e) {
-        } catch (IOException e) {
+            test = ((Deadline) tasks.get(tasks.size() - 1)).getFormattedDate();
+        } catch (DukeException | IOException e) {
+            throw new DukeException("Date format is incorrect");
         }
         Assertions.assertEquals(test, "05 Aug 2019");
     }
-
 }
